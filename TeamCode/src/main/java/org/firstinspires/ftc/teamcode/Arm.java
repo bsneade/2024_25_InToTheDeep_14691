04936382d047e2441775a7d96fa9051c8bd4e62d
Ericrobotic <wuhoo900@gmail.com>
2024-11-03 13:35:37 -0500

    working on issue #14 -Make Arm stop when Viper Limit is activated.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -3,12 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 public class Arm extends LinearOpMode {
 
     private DcMotor armViper = null;
     private DcMotor armLift= null;
 
+    DigitalChannel digitalTouch;  // Digital channel Object
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -18,6 +20,13 @@ public class Arm extends LinearOpMode {
         //setdirection for motors
         armLift.setDirection(DcMotorSimple.Direction.FORWARD);
         armViper.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        // get a reference to our touchSensor object.
+        digitalTouch = hardwareMap.get(DigitalChannel.class, "armViperLimit");
+
+        digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+        telemetry.addData("DigitalTouchSensorExample", "Press start to continue...");
+
         //add telemetry
         telemetry.addData("Arm Lift Status", "initialized");
         telemetry.update();
@@ -29,6 +38,15 @@ public class Arm extends LinearOpMode {
         while (opModeIsActive()) {
             double armLiftPower = gamepad2.left_stick_y;
             double armViperPower = gamepad2.right_stick_y;
+
+            // button is pressed set power =0
+
+            if (digitalTouch.getState() == true) {
+                armViperPower = 0;
+                telemetry.addData("Viper Limit", "Activated");
+            } else {
+                telemetry.addData("Viper Limit", "Inactive");
+            }
 
             armLift.setPower(armLiftPower);
             armViper.setPower(armViperPower);
