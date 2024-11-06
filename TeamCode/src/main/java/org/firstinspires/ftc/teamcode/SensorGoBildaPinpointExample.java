b5a1a4a6fd876112ffd459e808a6fb3438429034
Brad Sneade <bsneade@gmail.com>
2024-11-05 21:14:43 -0500

    Copied relevant code from https://github.com/jdhs-ftc/sparkfun-otos-quickstart to support the pinpoint integration with roadrunner.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorGoBildaPinpointExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorGoBildaPinpointExample.java
@@ -22,6 +22,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.ftc.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mm14691/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mm14691/Arm.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.mm14691;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
