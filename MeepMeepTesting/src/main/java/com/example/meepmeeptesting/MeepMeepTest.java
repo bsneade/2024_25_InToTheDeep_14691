89fbe5fd98738624d8a436375a2756e7a37d77af
kovi <kovidhmonga@gmail.com>
2024-11-10 12:59:25 -0500

    Added code from SensorGoBildaPinpointExample, refer to that for aditional comments and info also removed unneeded file

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTest.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTest.java
@@ -8,7 +8,7 @@ import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTest {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(865);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
